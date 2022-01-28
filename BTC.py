971d37a1dad217ede8d0f2ff38e72fa4cd15b5fe
ToxicDEV <95045254+T0xicDEV@users.noreply.github.com>
2022-01-28 18:30:33 +0100

    Add files via upload

---
+++ BTC.py
@@ -0,0 +1,22 @@
+import os
+import time
+from turtle import goto
+import colorama
+import sys
+print("STARTING MINING BITKOJN")
+time.sleep(1.5)
+print("LOADING LOADING SCREEN WAIT")
+
+def typewriter(text, delay=0.1):
+  for letter in text:
+    print(letter, end='', flush=True)
+    time.sleep(delay)
+  print()
+    
+typewriter("▁ ▂ ▃ ▄ ▅ ▆ ▇ █ ▇ ▆ ▅ ▄ ▃ ▁", 0.09)
+time.sleep(2.0)
+print()
+print("MINING BITKOINS HASH RAte 6969696969")
+time.sleep (6.9)
+print("ya you mine 1 bitkoin")
+print("Join discord.gg/batcore and use  batcore.eu/affiliate/Toxic")
